8d22d87c348e986573d519fa7e181071624f43c5
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-09 14:52:52 +0900

    Revert "[Edit] using csv manager and apply"
    This reverts commit 66e035dcc73191f69b58bc16d0748f44cc4d9863.

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import PySimpleGUI as gui
-from CsvManager import readCsv, getHeader, getValues
 
 def make_window(csvPath):
     input_path_layout=[[gui.Text("Insert Your CSV Path")], [gui.Input(key='-INPUT-'), gui.Button('Insert', size=(5,1))]]
-    output_path_layout = [[gui.Text(size=(100,1), key='-OUTPUT-', text=csvPath, auto_size_text=True)]]
-    values = ['No Data']
-    header = ['Error']
-    is_visible = False
-    if csvPath != "":
-        data = readCsv(csvPath)
-        header = getHeader(data)
-        values = getValues(data)
-        is_visible = True
-        
-    
-    table_layout = [[gui.Table(values, headings=header, visible=is_visible, num_rows=30, auto_size_columns=True, header_background_color='green')]]
+    output_path_layout = [[gui.Text(size=(40,1), key='-OUTPUT-', text=csvPath)]]
     layout = [[gui.Column(input_path_layout)],
-          [gui.Column(output_path_layout)],
-          [gui.Column(table_layout)]]
+          [gui.Column(output_path_layout)]]
     return gui.Window('Window Title', layout)
 
 
