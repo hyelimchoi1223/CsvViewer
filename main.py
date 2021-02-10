4fd3dbbb62e1675ee197826f109b2e5df5beb484
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-10 10:05:07 +0900

    [Edit] 테이블 표시 여부 수정, 버튼 이름 'Load'로 변경

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import PySimpleGUI as gui
 from CsvManager import readCsv, getHeader, getValues
 
 def make_window(csvPath):
-    input_path_layout=[[gui.Text("Insert Your CSV Path")], [gui.Input(key='-INPUT-'), gui.Button('Insert', size=(5,1))]]
+    input_path_layout=[[gui.Text("Insert Your CSV Path")], [gui.Input(key='-INPUT-'), gui.Button('Load', size=(5,1))]]
     output_path_layout = [[gui.Text(size=(100,1), key='-OUTPUT-', text=csvPath, auto_size_text=True)]]
     values = ['No Data']
     header = ['Error']
@@ -11,14 +11,16 @@ def make_window(csvPath):
         data = readCsv(csvPath)
         header = getHeader(data)
         values = getValues(data)
-        is_visible = True
-        
-    
-    table_layout = [[gui.Table(values, headings=header, visible=is_visible, num_rows=30, auto_size_columns=True, header_background_color='green')]]
+  
     layout = [[gui.Column(input_path_layout)],
-          [gui.Column(output_path_layout)],
-          [gui.Column(table_layout)]]
-    return gui.Window('Window Title', layout)
+          [gui.Column(output_path_layout)]]
+    
+    if is_visible :
+        table_layout = [[gui.Table(values, headings=header, visible=is_visible, num_rows=30, auto_size_columns=True, header_background_color='green')]]
+        layout.append([gui.Column(table_layout)])
+    
+    
+    return gui.Window('Csv Viewer', layout)
 
 
 window = make_window("") 
@@ -28,7 +30,7 @@ while True:
     if event == gui.WINDOW_CLOSED or event == 'Quit':
         break
     # Output a message to the window
-    if event == 'Insert':
+    if event == 'Load':
         window.close()
         window = make_window(values['-INPUT-'])        
 window.close()           
